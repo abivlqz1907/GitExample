f0db542e06c3574c5a15958cd4c26ad790f6f8f6
abivlqz1907 <aby1907@hotmail.com>
2020-06-22 16:44:25 -0600

    Initial Commit

---
+++ GitExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitExample
+//
+//  Created by Aby Velazquez on 22/06/20.
+//  Copyright © 2020 Aby Velazquez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
